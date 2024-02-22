3fc76ae2773594c4ce70c0a16f0d3b989038e894
Ihor Zhuk <ihorzhuk55555@icloud.com>
2024-02-22 19:29:42 +0200

    added mediumpurpl

--- src/script.js
+++ src/script.js
@@ -8,7 +8,9 @@ const scene = new THREE.Scene();
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "purple" });
+const material = new THREE.MeshBasicMaterial({
+  color: "mediumpurple",
+});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
