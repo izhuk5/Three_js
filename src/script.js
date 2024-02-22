85f5a2f5fc253c98ab54a5601e8687e535cddf9f
Ihor Zhuk <ihorzhuk55555@icloud.com>
2024-02-22 16:08:09 +0200

    base changes

--- src/script.js
+++ src/script.js
@@ -6,25 +6,19 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
-/**
- * Object
- */
+// Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
   width: 800,
   height: 600,
 };
 
-/**
- * Camera
- */
+// Camera
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height
@@ -32,9 +26,7 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 3;
 scene.add(camera);
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
