b5a1f424c70b3d1f0d6e96f072021f26f18ea965
Ihor Zhuk <ihorzhuk55555@icloud.com>
2024-03-10 17:40:53 +0200

    base

--- src/script.js
+++ src/script.js
@@ -21,10 +21,7 @@ const sizes = {
 };
 
 // Camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height
-);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
 
