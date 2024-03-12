187b8e2d0749d352ef934ed1566ebc33d4fddcbb
Ihor Zhuk <ihorzhuk55555@icloud.com>
2024-03-12 18:48:20 +0200

    base

--- src/script.js
+++ src/script.js
@@ -18,12 +18,45 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load("/textures/particles/2.png");
 
-/**
- * Test cube
- */
+// Particles
+
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 5000;
+
+const positions = new Float32Array(count * 3);
+
+for (let i = 0; i < count * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10;
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+);
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.05,
+  transparent: true,
+  color: "mediumpurple",
+  alphaMap: particleTexture,
+  // alphaTest: 0.0001,
+  // depthTest: false,
+  depthWrite: false,
+});
+
+particlesMaterial.blending = THREE.AdditiveBlending;
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+
+// Cube
 const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.BoxGeometry(),
   new THREE.MeshBasicMaterial()
 );
 scene.add(cube);
